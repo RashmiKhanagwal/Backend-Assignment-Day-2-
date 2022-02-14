df1480e699ef4145ce7c5de6b91f2f1ce7874a54
RashmiKhanagwal <rashmi.khanagwal@gmail.com>
2022-02-14 14:55:22 +0530

    server file

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Todo = require("./routes/todo"); 
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+mongoose
+  .connect('mongodb://localhost/Todolist', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+app.use("/todo",Todo);
+
+app.listen(3000, () => {console.log("server connected")});
